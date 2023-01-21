2ec688ee75fa3f18b3c83d71bec15d0b8d27e6df
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-21 11:09:06 +0100

    commit

--- practical.py
+++ practical.py
@@ -1,2 +1,6 @@
 hights = [100, 2, 300, 10, 11]
-largest_number = hights[]
+largest_number = hights[0]
+for nums in hights:
+    if nums > largest_number:
+        largest_number = nums
+        print(largest_number)
