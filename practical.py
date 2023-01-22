c400c291ff72c3133d6fefa928f38a32622273a2
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-22 20:54:41 +0930

    commit

--- practical.py
+++ practical.py
@@ -1,9 +1,14 @@
 myList = [10, 30, 5, 100, 56]
+s = slice(3)
+print(myList[s])
+for x in myList:
+    print(x)
 # print(max(myList))
 # print(len(myList))
 # print(myList[::-1])
 # print(myList[  :2  ][::-1])
-print(myList[  2:  ])
+# print(myList[  2:  ])
+print(myList[-1])
 def highestValue(arg):
     biggestValue = arg[0]
     for num in arg:
@@ -14,3 +19,8 @@ def highestValue(arg):
         
         
 # highestValue(myList)
+
+
+y = 'str'
+print(type(int('6')))
+print(range(10))
