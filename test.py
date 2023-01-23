957e69603b676c8a71590884f072df28913edf11
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-24 08:38:41 +0930

    commit

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-x = [1,3,5,69,04];
+x = [1,3,5,69,4];
 def myfunc(arg):
     higherVal = arg[0];
     for num in arg:
         if num > higherVal:
-            num = higherVal
+            higherVal = num
+    print(higherVal)
+    
+    
+myfunc(x)
