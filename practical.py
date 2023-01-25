7f965f2040eabb6c3634a0b56c203d0b8ea90e76
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-25 22:55:36 +0100

    commit

--- practical.py
+++ practical.py
@@ -1,14 +1,14 @@
 myList = [10, 30, 5, 100, 56]
-s = slice(3)
-print(myList[s])
-for x in myList:
-    print(x)
+# s = slice(3)
+# print(myList[s])
+# for x in myList:
+#     print(x)
 # print(max(myList))
 # print(len(myList))
 # print(myList[::-1])
 # print(myList[  :2  ][::-1])
 # print(myList[  2:  ])
-print(myList[-1])
+# print(myList[-1])
 
 def get_highest_value(arg):
     highestVal = arg[0]
@@ -19,7 +19,8 @@ def get_highest_value(arg):
             
 
 
-y = 'str'
-print(type(int('6')))
-print(range(10))
-print('hello world')
+get_highest_value(myList)
+# y = 'str'
+# print(type(int('6')))
+# print(range(10))
+# print('hello world')
