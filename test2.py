22ee7651e8fe803c798c873499f90332ca7f91fc
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-26 06:55:56 +0930

    commit

--- test2.py
+++ test2.py
@@ -1,17 +1,3 @@
-x = 4 
-#x is a variable that hold number 4
-y = 4
-
-"""
-this is a logic that
-explain how if 
-statement works in python
-gsrfytuiop
-dfghjkl;'
-dfghjkl;
-"""
-
-if x == y:
-    print('equal')
-else:
-    print('not equal')
+x = 5 
+y = 10
+print(x + y)
