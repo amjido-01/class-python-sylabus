11035e1c2bf11e092df5da056e3d0c1c92e9ed30
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-26 05:50:11 +0930

    commit

--- test2.py
+++ test2.py
@@ -1,6 +1,18 @@
-x = 10
+x = 4
 y = 2
-if y > x:
-    print(y , ' is smaller number')
-else:
-    print(x , ' is bigger number')
+
+
+# if x > y:
+#     print(x, 'is greater than ', y)
+# else:
+#     print(x, 'is not greater than y')
+    
+    
+def add_num(num1, num2):
+    return num1 + num2
+
+
+result = add_num(x, y)
+print(result)
+    
+    
