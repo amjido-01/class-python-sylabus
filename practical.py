dbfb5fd53e75744e6a90a092da8f49c5702aeff7
alajido <96017433+Alajido@users.noreply.github.com>
2023-01-31 22:20:42 +0100

    commit

--- practical.py
+++ practical.py
@@ -1,4 +1,8 @@
-def my_function(s, e+1)
-    for num in range(s, e+1):
-        if num % 7 == 0 and num % 5 == 0:
-            return 
+#   python program that will find those numbers which are divisible by 7 and multiple of 5 between 1500 and 2700 both inclusive with 10 different ways
+# give 10 other ways
+numbers = []
+for i in range(1500, 2701):
+    if i % 7 == 0 and i % 5 == 0:
+        numbers.append(i)
+
+print(numbers)
