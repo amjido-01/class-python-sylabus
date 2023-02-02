ec4064c1306385495de2f6d16b3e8467c299da20
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-02 12:57:07 +0100

    commit

--- loop.py
+++ loop.py
@@ -1,11 +1,15 @@
-# sch_materials = ["pen", "biro", "books", "chairs", "board", "ruler"];
-# print(sch_materials[::-1])
 
-List_num = [1,2,3,4,5,6,7];
-total = 0;
-for x in List_num:
-    total = total + x;
-print(total)
 
-list_num2 = 1+2+3
-print(list_num2)
+
+# nums = [1,2,3,4]
+# for num in nums:
+#     if num == nums[1]:
+#         continue
+#     print(num)
+    
+nums = [1,2,3,4]
+for num in nums:
+    if num == 2:
+        continue
+    print(num)
+
