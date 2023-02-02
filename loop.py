1f37f327c29f8f94886295a2a7606a26a173606d
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-02 12:37:09 +0100

    commit

--- loop.py
+++ loop.py
@@ -1,10 +1,11 @@
-sch_materials = ["pen", "biro", "books", "chairs", "board", "ruler"];
-num = [1,2,3,4,5]
-list_b = [1,23,4,5]
-for sule in sch_materials:
-    if sule.startswith('b'):
-        list_b.append(sule)
-print(list_b)
-for x in range(0, 101):
-    if x % 2 == 0:
-        print(x)
+# sch_materials = ["pen", "biro", "books", "chairs", "board", "ruler"];
+# print(sch_materials[::-1])
+
+List_num = [1,2,3,4,5,6,7];
+total = 0;
+for x in List_num:
+    total = total + x;
+print(total)
+
+list_num2 = 1+2+3
+print(list_num2)
