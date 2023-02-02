ef5e031c622a2321cd9f9f4124648492605e864c
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-02 15:09:11 +0100

    commit

--- loop.py
+++ loop.py
@@ -1,15 +1,25 @@
+# cost_per_ft = .87
+# user_input = int(input('How many fiber do you buy: '));
+# def total_fiber_cost(arg):
+#     resut = arg * cost_per_ft
+#     return resut
+# print(total_fiber_cost(user_input));
 
+# year = int(input('Enter a year:'))
+# def check_leaf_year(year):
+#     if (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0):
+#         print('leaf year')
+#         return
+#     print('not leaf year')
+            
+# check_leaf_year(year)
 
+# def divisible_by_7_and_multiple_of_5(start, end):
+#     result = []
+#     for i in range(start, end + 1):
+#         if i % 7 == 0 and i % 5 == 0:
+#             result.append(i)
+#     return result
 
-# nums = [1,2,3,4]
-# for num in nums:
-#     if num == nums[1]:
-#         continue
-#     print(num)
-    
-nums = [1,2,3,4]
-for num in nums:
-    if num == 2:
-        continue
-    print(num)
-
+# result = divisible_by_7_and_multiple_of_5(1500, 2700)
+# print(result)
