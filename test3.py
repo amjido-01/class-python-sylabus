bac062a2eeaff5c83a46ab750437e140f0fe8dfd
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-06 13:51:45 +0100

    commit

--- test3.py
+++ test3.py
@@ -43,20 +43,38 @@
 
 #parameter
 #argument
+# find those numbers between 1500 and 2700 
+# multiple of 5 and divisible of 7
+
+#looping -> interating
+#range -> range(start, end, step)
+#function -> 
+# for x in range(0, 10+1, 3):
+#     print(x)
+
+# values = [];
+# def my_fun(start, end):
+#     for x in range(start, end + 1):
+#         if x % 5 == 0 and x % 7 == 0:
+#             values.append(x)
+#     print(values)
 
 
-def hassan(first_name, surname):
-    full_name = first_name + surname
-    infor = "this a name of "
-    print(infor, full_name)
-    
-hassan("Hassan ", "Jibril")
-    
-    
+# my_fun(1500, 2700);
 
-# def faruq(first_name, last_name):
-#     faruq = first_name + last_name
-#     print(faruq)
+
+cost_fiber = 0.87;
+company_name = 'KUST'
+print(company_name)
+company_ft_length = int(input('Enter amount: '));
+
+def abdul(cost_fiber, company_name):
+    print(company_name)
+    result = company_ft_length * cost_fiber;
+    print(f'The total cost is {result} pr ft')
     
-# faruq("Faruq ", "Abubakar")
-        
+abdul(cost_fiber, company_name)
+
+
+
+
