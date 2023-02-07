37e4878bf6ae387fbc63737da7dc65424609593e
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-07 14:16:32 +0100

    commit

--- question.py
+++ question.py
@@ -1,8 +1,8 @@
-numbers = []
-def finding_numbers(begin, end):
-    for x in range(begin, end+1):
-        if x % 7 == 0 and x % 5 == 0:
-            numbers.append(x)
-    print(numbers)
+# numbers = []
+# def finding_numbers(begin, end):
+#     for x in range(begin, end+1):
+#         if x % 7 == 0 and x % 5 == 0:
+#             numbers.append(x)
+#     print(numbers)
 
-finding_numbers(1500, 2700)
+# finding_numbers(1500, 2700)
