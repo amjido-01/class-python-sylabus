9cfee38a0b0aa1ae3bb3c4c229fd95547d814b21
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-07 13:14:42 +0100

    commit

---
+++ question.py
@@ -0,0 +1,13 @@
+# looping
+# for x in range(11):
+#     print(x)
+    
+    
+    
+# numbers = [1,2,3,4,5,6,7,8,9,10];
+# for x in numbers:
+#     print(x)
+        
+yasir = ['yasir', 'sadik', 'umar', 'sule']
+for y in yasir:
+    print(y)
