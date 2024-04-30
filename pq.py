8560c6a27a2e83a51bfbb3f9d1d2f6cf62b687b8
amjido-01 <youndsadeeq10@gmail.com>
2024-04-30 17:50:29 +0100

    commit

--- pq.py
+++ pq.py
@@ -1,5 +1,7 @@
-nums = [1,2,3,4];
-num = [5,6,7,8];
-for i in nums:
-    for j in num:
-        print(i,j)
+nums = [1,2,3,4]
+num = [5,6,7,8]
+maxVal = num[0]
+for i in num:
+    if i > maxVal:
+        maxVal = i
+print(maxVal)
